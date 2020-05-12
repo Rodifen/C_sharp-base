d4f5fe9745cefcf827193ba64036c3d4b2b1ba3c
Rodifen <Rodifen@163.com>
2020-05-12 21:00:48 +0800

    fff

---
+++ 1/1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+#region RE
+
+#endregion
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           var re =  Console.ReadLine();
+           Console.Write(re);
+        }
+    }
+}
---
+++ CSBaser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSBaser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var re =    Console.ReadLine();
+            Console.WriteLine(re);
+        }
+    }
+}
